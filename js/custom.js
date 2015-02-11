37080f74dbe4b58cff02a47ece573ce7728aa882
Pekka Lammi <pekka.lammi@aalto.fi>
2015-02-11 21:14:34 +0200

    smooth anchor scrolling

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,22 @@
-var offset = 80;
+// var offset = 80;
 
-$('.navbar li a').click(function(event) {
-    event.preventDefault();
-    $($(this).attr('href'))[0].scrollIntoView();
-    scrollBy(0, -offset);
+// $('.navbar li a').click(function(event) {
+//     event.preventDefault();
+//     $($(this).attr('href'))[0].scrollIntoView();
+//     scrollBy(0, -offset);
+// });
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 80 // -80 koska offset
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
